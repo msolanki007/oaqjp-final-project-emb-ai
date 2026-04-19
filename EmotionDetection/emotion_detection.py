a1803b04cd3c6cc6faed954132f53509d8686f10
msolanki007 <mnsolanki786@gmail.com>
2026-04-19 12:51:41 -0400

    Initial Commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -13,15 +13,26 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } } 
 
     response = requests.post(url, json=myobj, headers=header)
-    formatted_response = json.loads(response.text)
-    emotion_dict = xyz = formatted_response['emotionPredictions'][0]['emotion']
-    first_val = list(emotion_dict.values())[0]
-    dominant_emotion = ''
-    for value in emotion_dict.values():
-        if value > first_val:
-            first_val = value
-    for key in emotion_dict:
-        if emotion_dict[key] == first_val:
-            dominant_emotion = key
-    emotion_dict.update({'dominant_emotion': dominant_emotion })
-    return emotion_dict
+    
+    if response.status_code == 200:
+
+        formatted_response = json.loads(response.text)
+        emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
+        first_val = list(emotion_dict.values())[0]
+        dominant_emotion = ''
+        for value in emotion_dict.values():
+            if value > first_val:
+                first_val = value
+        for key in emotion_dict:
+            if emotion_dict[key] == first_val:
+                dominant_emotion = key
+        emotion_dict.update({'dominant_emotion': dominant_emotion })
+        return emotion_dict
+    
+    elif response.status_code == 400:
+        emotion_dict = {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
+        return emotion_dict
+    else:
+        return None
+
+        
