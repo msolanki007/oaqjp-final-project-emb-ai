d69fddcbacf27598fabcacf6d7109e9ad3d35e76
msolanki007 <mnsolanki786@gmail.com>
2026-04-19 13:26:42 -0400

    Initial Commit

--- server.py
+++ server.py
@@ -1,23 +1,34 @@
+''' this is a server file to created to analize the sebtiment.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
-@app.route("/emotionDetector") 
-def sent_analyzer(): 
-    # Retrieve the text to analyze from the request arguments 
+@app.route("/emotionDetector")
+def sent_analyzer():
+    '''this will analyse the text and return the sentiment
+    '''
+    # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
-    # Pass the text to the sentiment_analyzer function and store the response 
+    # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+
+    text1 = "For the given statement, the system response is"
+    text2 = f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
+    text3 = f"'fear': {response['fear']}, 'joy': {response['joy']} "
+    text4 = f"and 'sadness': {response['sadness']}."
+    text5 = f"The dominant emotion is {response['dominant_emotion']}."
+    return f"{text1} {text2} {text3} {text4} {text5}"
 
 @app.route("/")
-def render_index_page(): 
+def render_index_page():
+    '''this will return default index page
+    '''
     return render_template('index.html')
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
